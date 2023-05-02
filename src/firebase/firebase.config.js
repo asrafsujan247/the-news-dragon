a5d29126e1ee0038ee1ee19c62545ba2930a2643
asrafsujan247 <asraf.sujan247@gmail.com>
2023-05-02 23:38:31 +0600

    initialized react router , installed firebase, added layout and pages components and setted some routes

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDl5cQGNjXwwUZspfwDzjvGYTekA4tw674",
+  authDomain: "the-news-dragon-4d693.firebaseapp.com",
+  projectId: "the-news-dragon-4d693",
+  storageBucket: "the-news-dragon-4d693.appspot.com",
+  messagingSenderId: "588335027762",
+  appId: "1:588335027762:web:b562f1a14e1953b8184e2b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="text-center">
+        <small>&#169; copyright of my news</small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
