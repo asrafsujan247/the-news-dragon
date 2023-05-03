594c20edcd0621d1f515603ca3bda312b1f1a6ae
asrafsujan247 <asraf.sujan247@gmail.com>
2023-05-03 16:36:20 +0600

    added latest news button and showed some text animation with react marquee and added navbar

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
@@ -12,7 +14,46 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-    </div>
+      <div className="d-flex gap-3 m-3">
+        <Button variant="danger">Latest</Button>
+        <Marquee speed={100}>
+          I can be a React component, multiple React components, or just some
+          text...... I can be a React component, multiple React components, or
+          just some text....
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="secondary">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
