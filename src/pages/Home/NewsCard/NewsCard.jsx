4533e0e3b5465a49c202863167e89609936ad235
asrafsujan247 <asraf.sujan247@gmail.com>
2023-05-08 20:56:31 +0600

    displayed nes details and author details in card

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,18 +2,30 @@ import moment from "moment";
 import React from "react";
 import { Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {
+  FaEye,
+  FaRegBookmark,
+  FaRegStar,
+  FaShareAlt,
+  FaStar,
+} from "react-icons/fa";
+import Rating from "react-rating";
 
 const NewsCard = ({ news }) => {
-  const { _id, title, details, image_url, author } = news;
+  const { _id, title, details, image_url, author, total_view, rating } = news;
   return (
     <Card className="mb-4">
-      <Card.Header className="d-flex">
+      <Card.Header className="d-flex align-items-center">
         <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
-        <div>
-          <p>{author?.name}</p>
-          <p>
+        <div className="ps-2 flex-grow-1">
+          <p className="mb-0">{author?.name}</p>
+          <p className="mb-0">
             <small>{moment(author?.published_date).format("yyyy-MM-D")}</small>
           </p>
+        </div>
+        <div>
+          <FaRegBookmark className="me-2"></FaRegBookmark>
+          <FaShareAlt></FaShareAlt>
         </div>
       </Card.Header>
       <Card.Body>
@@ -30,7 +42,26 @@ const NewsCard = ({ news }) => {
           )}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted d-flex align-items-center">
+        <div className="flex-grow-1">
+          <Rating
+            placeholderRating={rating?.number}
+            readonly
+            emptySymbol={
+              <FaRegStar className="text-warning fs-5 me-1"></FaRegStar>
+            }
+            placeholderSymbol={
+              <FaStar className="text-warning fs-5 me-1"></FaStar>
+            }
+            fullSymbol={<FaStar className="text-warning fs-5 me-1"></FaStar>}
+          />
+          <span className="ms-1">{rating?.number}</span>
+        </div>
+        <div>
+          <FaEye className="fs-5"></FaEye>
+          <span className="ms-2">{total_view}</span>
+        </div>
+      </Card.Footer>
     </Card>
   );
 };
--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { useLoaderData } from "react-router-dom";
 
 const News = () => {
+  const news = useLoaderData();
+
   return (
     <div>
       <h2>News details coming soon..</h2>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
